5efc8edca158922976ee6a2c38854c3f65c218f9
Ryujeonggeun <161789810+Ryujeonggeun@users.noreply.github.com>
2024-06-19 17:22:38 +0900

    Initial commit

---
+++ src/main/java/com/sparta/outsorcingproject/OutsorcingProjectApplication.java
@@ -0,0 +1,13 @@
+package com.sparta.outsorcingproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OutsorcingProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OutsorcingProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/sparta/outsorcingproject/OutsorcingProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sparta.outsorcingproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OutsorcingProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
