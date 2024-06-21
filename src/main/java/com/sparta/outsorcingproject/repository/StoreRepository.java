e7e5ef25f64905387178c3cf06ff778c1dd7556c
MoJun00 <167170478+MoJun00@users.noreply.github.com>
2024-06-21 15:06:18 +0900

    ✨ update:
    Menu Controller, Service, Repository 업데이트
    로그인한 유저가 소유한 가게인지 검사 기능 추가
    MenuResponseDto에 id 값 추가

--- src/main/java/com/sparta/outsorcingproject/repository/StoreRepository.java
+++ src/main/java/com/sparta/outsorcingproject/repository/StoreRepository.java
@@ -5,16 +5,20 @@ import org.springframework.context.MessageSource;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Locale;
+import java.util.Optional;
 
 public interface StoreRepository extends JpaRepository<Store, Long> {
 
+    /** 존재하지 않는 가게 **/
     default Store findStoreById(Long id, MessageSource messageSource) {
         return findById(id).orElseThrow(()->new IllegalArgumentException(messageSource.getMessage("not.find.store",null, Locale.getDefault())));
     }
-    /*default Store findStoreById(Long storeId,Long userId, MessageSource messageSource) {
-        Store store = findById(storeId).orElseThrow(()->new IllegalArgumentException(messageSource.getMessage("not.find.store",null, Locale.getDefault())));
+
+    /** 존재하지 않는 가게 + 유저가 가게의 소유주가 아닌 경우 오류 **/
+    default Store findStoreById(Long storeId,Long userId, MessageSource messageSource) {
+        Store store = findStoreById(storeId, messageSource);
         if(store.getUser().getId() != userId)
-            new IllegalArgumentException("해당 유저가 소유한 가게가 아닙니다.");
+            throw new IllegalArgumentException(messageSource.getMessage("mismatch.user",null, Locale.getDefault()));
         return store;
-    }*/
+    }
 }
