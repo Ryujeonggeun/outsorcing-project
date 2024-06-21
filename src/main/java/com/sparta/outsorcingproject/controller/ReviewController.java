53edcc4fcfe3e25b95eac36734979d08eda513e1
Ryujeonggeun <161789810+Ryujeonggeun@users.noreply.github.com>
2024-06-21 16:32:11 +0900

    ♻️ Refactor(리뷰엔티티): Validation Exception핸들러 추가

--- src/main/java/com/sparta/outsorcingproject/controller/ReviewController.java
+++ src/main/java/com/sparta/outsorcingproject/controller/ReviewController.java
@@ -5,6 +5,7 @@ import com.sparta.outsorcingproject.dto.ReviewResponseDto;
 import com.sparta.outsorcingproject.security.UserDetailsImpl;
 import com.sparta.outsorcingproject.service.ReviewService;
 import jakarta.validation.Valid;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
@@ -12,6 +13,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+@Slf4j
 @RestController
 @RequestMapping("/review")
 public class ReviewController {
@@ -22,18 +24,18 @@ public class ReviewController {
     //리뷰 등록
     //인증 인가 완성 되면 UserDetails 에서 뽑아와야함
     @PostMapping("/{ordersId}")
-    public ResponseEntity<String> addReview(@Valid @PathVariable Long ordersId, @RequestBody ReviewRequestDto requestDto,
-                                            @AuthenticationPrincipal UserDetailsImpl userDetails ) {
-    return  reviewService.addReview(ordersId,requestDto,userDetails.getUser());
+    public ResponseEntity<String> addReview(@PathVariable Long ordersId, @Valid @RequestBody ReviewRequestDto requestDto,
+                                            @AuthenticationPrincipal UserDetailsImpl userDetails) {
+        return reviewService.addReview(ordersId, requestDto, userDetails.getUser());
     }
 
 
     // 리뷰 수정
     //인증 인가 완성 되면 UserDetails 에서 뽑아와야함
     @PutMapping("/{reviewId}")
-    public ResponseEntity<String> updateReview(@PathVariable Long reviewId, @RequestBody ReviewRequestDto requestDto,
-                                                @AuthenticationPrincipal UserDetailsImpl userDetails)
-    {return reviewService.updateReview(reviewId, requestDto,userDetails.getUser());
+    public ResponseEntity<String> updateReview(@PathVariable Long reviewId, @Valid @RequestBody ReviewRequestDto requestDto,
+                                               @AuthenticationPrincipal UserDetailsImpl userDetails) {
+        return reviewService.updateReview(reviewId, requestDto, userDetails.getUser());
     }
 
     // 단일 리뷰 조회
@@ -52,7 +54,8 @@ public class ReviewController {
     // 리뷰 삭제
     //인증 인가 완성 되면 UserDetails 에서 뽑아와야함
     @DeleteMapping("/{reviewId}")
-    public ResponseEntity<String> deleteReview(@PathVariable Long reviewId, @AuthenticationPrincipal UserDetailsImpl userDetails) {
+    public ResponseEntity<String> deleteReview(@PathVariable Long
+                                                       reviewId, @AuthenticationPrincipal UserDetailsImpl userDetails) {
         return reviewService.deleteReview(reviewId, userDetails.getUser());
     }
 
