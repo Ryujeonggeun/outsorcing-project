d6c2587a920b582724a63e5d7a6fe4504e062609
Ryujeonggeun <161789810+Ryujeonggeun@users.noreply.github.com>
2024-06-23 18:40:44 +0900

    🎉 Feat(Admin): 유저 권한변경 완성

--- src/main/java/com/sparta/outsorcingproject/admin/AdminController.java
+++ src/main/java/com/sparta/outsorcingproject/admin/AdminController.java
@@ -17,17 +17,18 @@ public class AdminController {
 
     //유저 전체 조회
     @GetMapping("/users")
-    public ResponseEntity<List<UserResponseDto>> getAllUsers(@AuthenticationPrincipal UserDetailsImpl userDetails) {
+    public ResponseEntity<List<UserResponseDto>> getAllUsers() {
         List<UserResponseDto> allUsers = adminService.getAllUsers();
         return ResponseEntity.ok(allUsers);
     }
 
     //사용자 권한 수정 삭제
     @PutMapping("/user/{userId}/role")
-    public ResponseEntity<String> updateUserRole(@PathVariable Long userId, @RequestParam String role,
+    public ResponseEntity<String> updateUserRole(@PathVariable Long userId, @RequestParam String newRole,
                                                  @AuthenticationPrincipal UserDetailsImpl userDetails) {
 
-        return ResponseEntity.ok("권한 수정 되었습니다");
+        adminService.updateUserRole(userId,newRole,userDetails.getUser());
+        return ResponseEntity.ok(userId + "유저의 권한이 " + newRole + " 으로 수정되었습니다.");
     }
 
     //메뉴 전체 조회
--- src/main/java/com/sparta/outsorcingproject/admin/AdminService.java
+++ src/main/java/com/sparta/outsorcingproject/admin/AdminService.java
@@ -1,19 +1,63 @@
 package com.sparta.outsorcingproject.admin;
 
+import com.sparta.outsorcingproject.entity.User;
+import com.sparta.outsorcingproject.entity.UserRoleEnum;
 import com.sparta.outsorcingproject.repository.UserRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.MessageSource;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
+import java.util.Locale;
 
 @Service
 public class AdminService {
 
+    private static final Logger log = LoggerFactory.getLogger(AdminService.class);
     @Autowired
     UserRepository userRepository;
+
+    @Autowired
+    MessageSource messageSource;
 
     public List<UserResponseDto> getAllUsers() {
         return userRepository.findAll().stream()
                 .map(UserResponseDto::new).toList();
     }
+
+    @Transactional
+    public void updateUserRole(Long userId, String newRole, User user) {
+
+        //대문자로 변경
+       newRole = newRole.toUpperCase();
+
+        // ADMIN/User 둘 중 하나로 들어왔는지 체크
+        if (!("USER".equals(newRole) | "ADMIN".equals(newRole))) {
+            throw new IllegalArgumentException("잘못된 값을 입력하셨습니다.\n가능한 입력: user/admin");
+        }
+
+        //모양 맞춰주기
+        newRole = "ROLE_" + newRole;
+
+        // userId로 user 찾기
+        User getUser = userRepository.findById(userId).orElseThrow(
+                () -> new IllegalArgumentException(
+                        messageSource.getMessage("not.find.user", null, Locale.getDefault())
+                )
+        );
+
+        // 바꾸려는 권한이 원래 있던 권한가 같으면 예외 처리
+        if (newRole.equals(getUser.getRole().getAuthority())) {
+        throw new IllegalArgumentException("바꾸려는 유저의 권한은이미 " + newRole + "입니다");
+        }
+
+        //변경후 누가 변경했는지 로그 찍기
+        UserRoleEnum userRoleEnum = UserRoleEnum.valueOf(newRole.substring(5));
+        getUser.setRole(userRoleEnum);
+        log.info(user.getUsername() + "가 UserId: " + userId + " 의 권한을 " + newRole + " 로 변경하였습니다." );
+        }
+
 }
