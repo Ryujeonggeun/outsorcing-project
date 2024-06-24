858c4324770dbacc29e57605e685c7c07b304b2b
K <xv40101392@gmail.com>
2024-06-24 12:30:03 +0900

    🩹 fix(리팩토링):ProfileModifyRequestDto 필요없는 username 요청 수청

--- src/main/java/com/sparta/outsorcingproject/dto/ProfileModifyRequestDto.java
+++ src/main/java/com/sparta/outsorcingproject/dto/ProfileModifyRequestDto.java
@@ -4,7 +4,7 @@ import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Pattern;
 import lombok.Getter;
 import lombok.Setter;
-import org.springframework.security.crypto.password.PasswordEncoder;
+
 
 @Getter
 @Setter
